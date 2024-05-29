440f45e3c560d5a71ae23aff951577d365c21891
senapathisowjanya <sowjanyanageswar9@gmail.com>
2024-05-29 14:28:35 +0530

    changes done

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,9 @@ const { connectDB } = require("./config/db")
 const app = express()
 const cors = require("cors")
 
-
 app.use(cors());
 app.use(express.static("uploads"))
+
 app.use(express.json())
 app.use(express.json({ limit: "100mb" }));
 
